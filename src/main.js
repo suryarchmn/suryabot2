2deb9af31325980cd6b609ab741cc162e042f7fd
suryarchmn <32777860+suryarchmn@users.noreply.github.com>
2017-10-16 21:31:14 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -128,29 +128,29 @@ class LINE extends Command {
         
         this.command('uy', ['uy jua','siapa nih?']);
         this.command('ikam siapa?', this.getProfile.bind(this));
-        this.command('.status', `Your Status: ${JSON.stringify(this.stateStatus)}`);
-        this.command(`.left ${payload}`, this.leftGroupByName.bind(this));
-        this.command('.speed', this.getSpeed.bind(this));
-        this.command('.kernel', this.checkKernel.bind(this));
-        this.command(`kick ${payload}`, this.OnOff.bind(this));
-        this.command(`cancel ${payload}`, this.OnOff.bind(this));
-        this.command(`qrp ${payload}`, this.OnOff.bind(this));
-        this.command(`.kickall ${payload}`,this.kickAll.bind(this));
-        this.command(`.cancelall ${payload}`, this.cancelMember.bind(this));
-        this.command(`.sur`,this.setReader.bind(this));
-        this.command(`.sider sur`,this.rechecks.bind(this));
-        this.command(`.clearall`,this.clearall.bind(this));
-        this.command('.myid',`Your ID: ${messages.from}`)
-        this.command(`.ip ${payload}`,this.checkIP.bind(this))
-        this.command(`.ig ${payload}`,this.checkIG.bind(this))
-        this.command(`.qr ${payload}`,this.qrOpenClose.bind(this))
-        this.command(`.joinqr ${payload}`,this.joinQr.bind(this));
-        this.command(`.spam ${payload}`,this.spamGroup.bind(this));
-        this.command(`.creator`,this.creator.bind(this));
+        this.command('/status', `Your Status: ${JSON.stringify(this.stateStatus)}`);
+        this.command(`/left ${payload}`, this.leftGroupByName.bind(this));
+        this.command('/speed', this.getSpeed.bind(this));
+        this.command('/kernel', this.checkKernel.bind(this));
+        this.command(`/kick ${payload}`, this.OnOff.bind(this));
+        this.command(`/cancel ${payload}`, this.OnOff.bind(this));
+        this.command(`/qrp ${payload}`, this.OnOff.bind(this));
+        this.command(`/kickall ${payload}`,this.kickAll.bind(this));
+        this.command(`/cancelall ${payload}`, this.cancelMember.bind(this));
+        this.command(`sur`,this.setReader.bind(this));
+        this.command(`sider sur`,this.rechecks.bind(this));
+        this.command(`/clearall`,this.clearall.bind(this));
+        this.command('/myid',`Your ID: ${messages.from}`)
+        this.command(`/ip ${payload}`,this.checkIP.bind(this))
+        this.command(`/ig ${payload}`,this.checkIG.bind(this))
+        this.command(`/qr ${payload}`,this.qrOpenClose.bind(this))
+        this.command(`/joinqr ${payload}`,this.joinQr.bind(this));
+        this.command(`/spam ${payload}`,this.spamGroup.bind(this));
+        this.command(`/creator`,this.creator.bind(this));
 
-        this.command(`pap ${payload}`,this.searchLocalImage.bind(this));
-        this.command(`.upload ${payload}`,this.prepareUpload.bind(this));
-        this.command(`vn ${payload}`,this.vn.bind(this));
+        this.command(`/pap ${payload}`,this.searchLocalImage.bind(this));
+        this.command(`/upload ${payload}`,this.prepareUpload.bind(this));
+        this.command(`/vn ${payload}`,this.vn.bind(this));
 
         if(messages.contentType == 13) {
             messages.contentType = 0;
